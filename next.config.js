02c97ecb05cc98d9c21b6e08f867605c0916d951
yuma-nanjo <y.nanjo1110@gmail.com>
2022-08-05 11:25:26 +0900

    fource commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ["images.unsplash.com"],
   },
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    optimizeFonts: true,
-  },
 };
 
 module.exports = nextConfig;
