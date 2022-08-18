f568e0164ef8bc24053bad57e2e934e639347db1
yuma-nanjo <y.nanjo1110@gmail.com>
2022-08-18 17:48:19 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,7 @@ export default function Home(data) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const user_name = "sawa_officialgram"; //ビジネスorクリエイターアカウントの必要あり
   const access_token = process.env.NEXT_PUBLIC_INSTAGRAMTOKEN;
   const user_id = process.env.NEXT_PUBLIC_INSTAGRAMID;
